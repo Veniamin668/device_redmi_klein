219f186402e0386d3387850963d0b757b64b7724
ktoya? <venik1821@gmail.com>
2026-08-08 12:06:24 +0500

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,7 +6,7 @@ echo "===================================================="
 echo "   🔥 ULTIMATE UNISOC PATCHER FOR REDMI A3X 🔥     "
 echo "===================================================="
 
---- 1. Наш графический фикс (прямая DRM) ---
+# --- 1. Наш графический фикс (прямая замена) ---
 if [ -f "$DEVICE_PATH/patch/graphics_drm.cpp.sprd_legacy_kms_fixed" ]; then
     echo "[*] Применение C++ DRM фикса графики..."
     cp -f "$DEVICE_PATH/patch/fix_drm.cpp" bootable/recovery/minui/graphics_drm.cpp
